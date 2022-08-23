65ca4d3f9be8f3ef3862a8ab992e33bb4a0a9a16
ngkayong <111838580+ngkayong@users.noreply.github.com>
2022-08-23 22:19:07 +0800

    Add files via upload

---
+++ quessing game.py
@@ -0,0 +1,56 @@
+import random
+
+def main():
+    print("welcome to our game")
+    print("Guess the answer")
+
+    list =['carbon','nitrogen','oxygen','flourine','helium','neon']
+    
+    print("this element is released into the atmosphere through respiration")
+    print(list)
+    answer = input("name the element with the elements above:")
+
+    print("the answer is carbon")
+    
+    print("second question:")
+    
+    print("this element is a common element in the universe,we breathe about 79.02% in the air")
+    print(list)
+    answer = input("name the element:")
+    
+    print("the answer is nitrogen")
+    
+    print("third question")   
+       
+    print("it is a diatomic gas and is what plants releasesduring photosynthesis")
+    print(list)
+    answer = input("name the element:")
+    
+    print("the answer is oxygen")
+    
+    print("fourth question")
+
+    print("this element existence is a highly toxic,pale yellow diatomic gas")
+    print(list)
+    answer = input("name the element:")
+       
+    print("the answer is flourine")
+    
+    print("firth qeustion")
+    
+    print("this element is used in rocket fuel")
+    print(list)
+    answer = input("name the element:")
+       
+    print("the answer is helium")
+    
+    print("sixth question")
+    
+    print("this element is a colorless,odorless noble gas")
+    print(list)
+    answer = input("name the element:")
+    
+       
+    print("the answer is neon")
+
+main()
